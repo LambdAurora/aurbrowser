48a58ba1095506e6484389f88b004646aa8453b7
LambdAurora <aurora42lambda@gmail.com>
2019-01-10 21:28:16 +0100

    :sparkles: Adding Vue.js

---
+++ views/components/loader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="preloader-wrapper big active">
+    <div class="spinner-layer spinner-blue-only">
+      <div class="circle-clipper left">
+        <div class="circle"></div>
+      </div>
+      <div class="gap-patch">
+        <div class="circle"></div>
+      </div>
+      <div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "loader"
+	}
+</script>
+
+<style scoped>
+  .preloader-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+  }
+</style>
