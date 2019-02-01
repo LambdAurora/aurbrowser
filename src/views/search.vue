a9c8d3cfaca27f0f628c452310c0b3b3fb92ae1e
LambdAurora <aurora42lambda@gmail.com>
2019-02-01 23:25:37 +0100

    :bug: Fixed freezing on search (RIP live search) and cleaned a bit the index page.

--- src/views/search.vue
+++ src/views/search.vue
@@ -6,7 +6,7 @@
           <v-combobox v-model="search_by" v-on:input="search" type="button" :items="search_by_items" label="Search by:" hide-details light></v-combobox>
         </v-flex>
         <v-flex class="xs9">
-          <v-text-field v-model="search_query" v-on:input="search" hide-details prepend-icon="search" label="Search" single-line light></v-text-field>
+          <v-text-field v-model="search_query" v-on:change="search" hide-details prepend-icon="search" label="Search" single-line light></v-text-field>
         </v-flex>
       </v-layout>
     </div>
